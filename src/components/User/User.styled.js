e32492fdd55a43654a067d74a814518f594d174a
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-01-15 20:12:09 +0200

    add styled variant

--- src/components/User/User.styled.js
+++ src/components/User/User.styled.js
@@ -26,3 +26,25 @@ export const SpanText = styled.span`
     color: #000000;
   }
 `;
+
+// export const EmailText = styled.p`
+//   font-size: 22px;
+//   color: orange;
+//   &:hover {
+//     background: deeppink;
+//   }
+// `
+
+// export const SpanText = styled.span`
+//   background: ${({ isRed }) => isRed ? 'red' : 'yellow'};
+//   color: #4d4a4a;
+// `
+
+// export const NameText = styled.p`
+//   font-size: 25px;
+//   color: #9a326a;
+//   &:hover ${SpanText} {
+//     color: aqua;
+
+//   }
+// `
